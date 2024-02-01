06318b7180c692736721db86a2cd115e76408dc8
nick-bennett <nbennett10@cnm.edu>
2024-02-01 14:34:53 -0700

    Initialization sequence.

--- src/main/java/edu/cnm/deepdive/animals/Canis.java
+++ src/main/java/edu/cnm/deepdive/animals/Canis.java
@@ -2,11 +2,24 @@ package edu.cnm.deepdive.animals;
 
 public abstract class Canis {
 
+  private static int helperValue = helper();
+
   private final String name;
 
+  static {
+    System.out.println("Canis static block");
+  }
+
+  {
+    System.out.println("Canis instance initialization block");
+  }
+
   protected Canis(String name) {
+    System.out.println("Canis::<init>");
     this.name = name;
   }
+
+  private final int instHelperValue = helper();
 
   public abstract void hunt();
 
@@ -28,5 +41,15 @@ public abstract class Canis {
     return "%1$s[name=%2$s]".formatted(getClass().getSimpleName(), name);
   }
 
+  private static int helper() {
+    System.out.println("Canis::helper");
+    return 0;
+  }
+
+  private static int helperValue2 = helper();
+
+  static {
+    System.out.println("Canis second static block");
+  }
 
 }
